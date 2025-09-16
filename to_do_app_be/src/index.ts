a1ba78f708de698cb7234c59246a239f0c241acf
[vy] <[vy.triphunter@gmail.com]>
2025-09-16 20:07:03 +0700

    ád

--- to_do_app_be/src/index.ts
+++ to_do_app_be/src/index.ts
@@ -35,7 +35,7 @@ AppDataSource.initialize()
 
     setupSwagger(app);
 
-    const feDistPath = path.join(__dirname, "");
+    const feDistPath = path.join(__dirname, "../client");
     app.use(express.static(feDistPath));
     app.use((req, res) => {
       res.sendFile(path.join(feDistPath, "index.html"));
